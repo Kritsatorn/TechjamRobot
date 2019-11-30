f906bd6d83ce1e3fdfe80167dd63d3b3ed1b5508
Kritsatorn <kritsatorn@billme.co.th>
2019-11-30 17:27:21 +0700

    get robots

--- index.js
+++ index.js
@@ -10,8 +10,37 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+let robots = [{"a":1}];
+let currentId = 0;
+
 // localhost:8080/greeting
 app.get("/greeting", function (request, response) {
   console.log("YAY! New request is coming!");
-  response.status(200).send("Hello world!");
+  response.status(200).send(robots);
 });
+
+// localhost:8080/robots
+app.get("/robots", function (request, response) {
+  response.status(200).send("robots");
+});
+
+// http://localhost:8080/distance
+app.post("/distance", function (request, response) {
+
+  const {
+    first_pos,
+    second_pos
+  } = request.body;
+
+  const newRobot = {
+    first_pos: first_pos,
+    second_pos: second_pos,
+    id: currentId
+  };
+  robots.push(newRobot)
+  currentId += 1;
+  console.log(a)
+  response.sendStatus(201);
+});
+
+
